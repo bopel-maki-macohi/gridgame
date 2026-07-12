13b700e6d25480f26299d857bc4e0a8dc1b9fbad
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-07-12 18:56:29 -0500

    random chance for it to not start growing

--- source/FunctionalGridTile.hx
+++ source/FunctionalGridTile.hx
@@ -16,9 +16,10 @@ class FunctionalGridTile extends GridTile
 				wheat_layer.animation.add('growth', [0, 1, 2], 1, false);
 				wheat_layer.animation.pause();
 				wheat_layer.animation.frameIndex = 0;
+				wheat_layer.visible = false;
 				graphic_layers.push(wheat_layer);
 
-				data.growth = 0;
+				data.growth = -1;
 				data.growth_max = 2;
 				data.growth_tick = getWaitTick(32, 10, 20);
 		}
@@ -34,13 +35,33 @@ class FunctionalGridTile extends GridTile
 				if (tick_value > data.untill_tick)
 					if (tick_random.bool(10))
 						setBlock('dirt');
+
 			case 'wheat':
-				if (data.growth < data.growth_max && tick_value > data.growth_tick)
-					if (tick_random.bool(10))
+				if (data.growth < data.growth_max)
+				{
+					if (tick_value > data.growth_tick && tick_random.bool(10))
 					{
-						data.growth++;
+						if (data.growth >= 0)
+							data.growth++;
+						else
+						{
+							if (tick_random.bool(25))
+								data.growth++;
+							else
+								data.growth--;
+						}
+
 						graphic_layers[0].animation.frameIndex = data.growth;
+						graphic_layers[0].visible = data.growth >= 0;
 					}
+
+					if (data.growth < -1)
+					{
+						final previous_tick = tick_value;
+						setBlock('dirt-tilled');
+						tick_value = tick_random.int(0, previous_tick);
+					}
+				}
 		}
 	}
 
